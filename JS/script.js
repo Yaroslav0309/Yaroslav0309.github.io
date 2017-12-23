db69fd83798063bac3105670d29b259db741a6a5
Yaroslav0309 <Yaroslav9d@gmail.com>
2017-12-23 15:56:07 +0200

    sum1

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-  var carousels = document.querySelectorAll('#started .carousel');
-  var currentCarousel = 0;
-  var carouselInterval = setInterval(nextCarousel,3000); /* Интервал между картинками */
-
-  function nextCarousel(){
-  carousels[currentCarousel].className = 'carousel';
-  currentCarousel = (currentCarousel+1)%carousels.length;
-  carousels[currentCarousel].className = 'carousel demonstration';
-}
